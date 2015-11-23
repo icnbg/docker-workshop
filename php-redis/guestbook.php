1117c52e34a60a751f76fc7de0fa699c387dd1a0
Borislav Borislavov <b.borislavov@icn.bg>
2015-11-23 18:58:32 +0200

    hostname change

--- php-redis/guestbook.php
+++ php-redis/guestbook.php
@@ -10,7 +10,7 @@ require 'Predis/Autoloader.php';
 Predis\Autoloader::register();
 
 if (isset($_GET['cmd']) === true) {
-  $host = 'redis-master';
+  $host = 'rmaster';
   if (getenv('GET_HOSTS_FROM') == 'env') {
     $host = getenv('REDIS_MASTER_SERVICE_HOST');
   }
@@ -25,7 +25,7 @@ if (isset($_GET['cmd']) === true) {
     $client->set($_GET['key'], $_GET['value']);
     print('{"message": "Updated"}');
   } else {
-    $host = 'redis-slave';
+    $host = 'rslave';
     if (getenv('GET_HOSTS_FROM') == 'env') {
       $host = getenv('REDIS_SLAVE_SERVICE_HOST');
     }
